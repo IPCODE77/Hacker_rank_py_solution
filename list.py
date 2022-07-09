4f9d102894076b21bc0602bc36307d71f9dd0333
ITISH KUMAR PATI <PATIITISHKUMAR@GMAIL.COM>
2022-07-09 10:26:52 +0530

    factorial of number

---
+++ list.py
@@ -0,0 +1,9 @@
+N=int(input("Enter input: "))
+list1=[]
+
+for i in range(N):
+    command=input("Enter command: ")
+    a=int(input("Enter The value of a index: "))
+    e=int(input("Enter The value of a: "))
+    if command=="insert":
+        list.insert(i,e)
